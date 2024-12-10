a528bba587910b9d90580e5a6d6f8e9b66592b58
Maedeh Khansari <khansari.maedeh@gmail.com>
2024-12-10 17:44:18 +0100

    second commit

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isChecked: boolean = false;
 
+  redLogo = 'img/logo/small-logo-red.png';
+  whiteLogo = 'img/logo/small-logo.png';
+
+  closeMenu() {
+    setTimeout(() => {
+      this.isChecked = false;
+    }, 100);
+  }
 }
